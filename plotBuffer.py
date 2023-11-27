f4b59510c878cea52c3546757b5848603e1a6845
Aditya-Bhardwaj-99 <bhardwaj.aditya99@gmail.com>
2023-11-27 17:42:00 +0530

    Changes to be committed: 	modified:   plotBuffer.py

--- plotBuffer.py
+++ plotBuffer.py
@@ -5,20 +5,30 @@ import time
 from matplotlib.widgets import CheckButtons, Button
 from matplotlib.animation import FuncAnimation
 
+def clear_all(what):
+    ds = pd.DataFrame({"Ammonia":[],"Benzene":[],"CO2":[],"Nitrox":[],"Alcohol":[]})
+    ds.to_csv('./BufferData.csv',index=False)
+    ds2 = pd.DataFrame({"Bill":[]})
+    ds2.to_csv('./BillData.csv',index=False)
+    ds3 = pd.DataFrame({"Trees":[]})
+    ds3.to_csv('./Trees.csv',index=False)
+
 ds = pd.read_csv('./BufferData.csv')
 ds2 = pd.read_csv('./BillData.csv')
 trees = pd.read_csv('./Trees.csv')
-bill_co2 = ds["CO2"].mean()
-ds3 = pd.DataFrame({"Bill":[bill_co2]})
-plant = bill_co2/200
-newTree = pd.concat([trees,pd.DataFrame({"Trees":[plant]})])
-ds2 = pd.concat([ds2,ds3])
-ds2.to_csv('./BillData.csv',index=False)
+if len(ds["CO2"])>0:
+    bill_co2 = ds["CO2"].mean()
+    ds3 = pd.DataFrame({"Bill":[bill_co2]})
+    plant = bill_co2/200
+    newTree = pd.concat([trees,pd.DataFrame({"Trees":[plant]})])
+    ds2 = pd.concat([ds2,ds3])
+    ds2.to_csv('./BillData.csv',index=False)
 trees = pd.read_csv('./Trees.csv')
 
 # figure, axis
 fig = plt.figure(num='abc', figsize=(10, 10))
 ax = fig.add_subplot(1,1,1)
+rax = fig.add_axes([0.03, 0.4, 0.1, 0.1])
 # fig.set_size_inches(12,8)
 if trees["Trees"].sum() >= 2:
     print_text="Over Polluting!! Please Plant trees"
@@ -41,6 +51,9 @@ lines = [fing1, fing2, fing3, fing4, thumb, cost]
 plt.subplots_adjust(left=0.25, bottom=0.1, right=0.95, top=0.95)
 
 Fingers = ["Ammonia", "Benzene", "CO2", "Nitrox", "Alcohol", "Bill"]
+btn = Button(rax,"Clear All")
+btn.on_clicked(clear_all)
+
 # activated = [True, True, True, True, True]
 # Checkbox = plt.axes([0.03, 0.4, 0.15, 0.20])
 # chkbox = CheckButtons(Checkbox, Fingers, activated)
@@ -55,16 +68,14 @@ def animate(i):
     ds = pd.read_csv('./BufferData.csv')
     ds2 = pd.read_csv('./BillData.csv')
     trees = pd.read_csv('./Trees.csv')
-    bill_co2 = ds["CO2"].mean()
-    ds3 = pd.DataFrame({"Bill":[bill_co2]})
-    plant = bill_co2/200
-    newTree = pd.concat([trees,pd.DataFrame({"Trees":[plant]})])
-    print(ds2)
-    ds2 = pd.concat([ds2,ds3])
-    print(ds2)
-    
-    ds2.to_csv('./BillData.csv',index=False)
-    newTree.to_csv("./Trees.csv",index=False)
+    if len(ds["CO2"])>0:
+        bill_co2 = ds["CO2"].mean()
+        ds3 = pd.DataFrame({"Bill":[bill_co2]})
+        plant = bill_co2/200
+        newTree = pd.concat([trees,pd.DataFrame({"Trees":[plant]})])
+        ds2 = pd.concat([ds2,ds3])
+        ds2.to_csv('./BillData.csv',index=False)
+        newTree.to_csv("./Trees.csv",index=False)
     trees = pd.read_csv('./Trees.csv')
     ax.clear()
     if trees["Trees"].sum() >= 2:
